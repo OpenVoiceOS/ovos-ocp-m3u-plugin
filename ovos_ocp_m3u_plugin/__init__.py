d0e9d162805c176b55cd43c5482b633dc36b8edd
jarbasai <jarbasai@mailfence.com>
2022-08-07 16:10:04 +0100

    :tada:

--- ovos_ocp_m3u_plugin/__init__.py
+++ ovos_ocp_m3u_plugin/__init__.py
@@ -16,10 +16,9 @@ class OCPPlaylistExtractor(OCPStreamExtractor):
     @staticmethod
     def get_playlist_stream(uri):
         # .pls and .m3u are not supported by gui player, parse the file
-        if "pls" in uri or ".m3u" in uri:
-            txt = requests.get(uri).text
-            for l in txt.split("\n"):
-                if l.startswith("http"):
-                    return {"uri": l}
+        txt = requests.get(uri).text
+        for l in txt.split("\n"):
+            if l.startswith("http"):
+                return {"uri": l}
         return {"uri": uri}
 
